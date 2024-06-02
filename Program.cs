9204223a5ea7ca804fc51683a55cf334c3d5790a
Pandora-boxes <lingogreyling@gmail.com>
2024-06-02 15:27:56 +0200

    Added a way to get the filepath of the ApplicantData.txt file

--- Program.cs
+++ Program.cs
@@ -9,8 +9,41 @@ namespace Arcade_app
 {
     internal class Program
     {
+        static void ApplicantDataEntry(string filepath)
+        {
+
+            bool enter = true;
+            do
+            {
+
+            } while (enter ==true);
+        }
+
         static void Main(string[] args)
         {
+            string filePath = Directory.GetCurrentDirectory();
+            List<string> filepatharr = new List<string>(filePath.Split('\\'));
+
+            Console.WriteLine(filePath);
+
+            for (int i = 0; i < filepatharr.Count;i++)
+            {
+                if (filepatharr[i] == ("Arcade_app"))
+                {
+                    filepatharr[i + 1] = "ApplicantData.txt";
+                    filepatharr.Remove("Debug");
+                    break;
+                }
+
+            }
+            
+            foreach (var path in filepatharr)
+            {
+                Console.WriteLine(path);
+            }
+            filePath = string.Join("\\", filepatharr);
+
+            Console.ReadKey();
         }
     }
 }

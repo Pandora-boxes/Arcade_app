18af00e10843b359857c0537a72c7e60646261d3
MrBooBooBear <fouriejooste@gmail.com>
2024-06-03 21:00:26 +0300

    Menu & bug fixes
    Added a actual working menu, plus fixed some spelling mistakes and deleted redundant code

--- Program.cs
+++ Program.cs
@@ -34,14 +34,14 @@ namespace Arcade_app
                 applicantDataArr.Add(applicantName);
                 formatCorrect = false;
 
-                Console.WriteLine("What is the applicant's age?");
+                Console.WriteLine("\nWhat is the applicant's age?");
                 while (formatCorrect == false)
                 {
                     applicantAge = Console.ReadLine();
                     formatCorrect = int.TryParse(applicantAge, out int Age);            //A tryParse into a throwaway variable like age to
                                                                                         //check if the string can be properly formatted into it's intended data type
                     if (formatCorrect == false)
-                        Console.WriteLine("The format is wrong you need to write an age.Try again.");
+                        Console.WriteLine("\nThe format is wrong you need to write an age.Try again.");
                     else if (formatCorrect == true)
                         applicantDataArr.Add(applicantAge);
                         
@@ -49,31 +49,31 @@ namespace Arcade_app
                                                                                         //so that if the user gets the data format wrong they can redo the data entry
 
                 formatCorrect = false;
-                Console.WriteLine("What is the applicant's high score rank?");
+                Console.WriteLine("\nWhat is the applicant's high score rank?");
                 while (formatCorrect == false)
                 { 
                     applicantHighScoreRank = Console.ReadLine();
                     formatCorrect = int.TryParse(applicantHighScoreRank, out int HighScoreRank);
                     if (formatCorrect == false)
-                        Console.WriteLine("The format is wrong you need to write an number rank.Try again.");
+                        Console.WriteLine("\nThe format is wrong you need to write an number rank.Try again.");
                     else if (formatCorrect == true)
                         applicantDataArr.Add(applicantHighScoreRank);
                 }
 
                 formatCorrect = false;
-                Console.WriteLine("What is the applicant's start date?: mm/dd/yyy");
+                Console.WriteLine("\nWhat is the applicant's start date?: mm/dd/yyyy");
                 while (formatCorrect == false)
                 {
                     applicantStartDate = Console.ReadLine();
                     formatCorrect = DateTime.TryParse(applicantStartDate, out DateTime StartDate);
                     if (formatCorrect == false)
-                        Console.WriteLine("The format is wrong you need to write a date.Try again");
+                        Console.WriteLine("\nThe format is wrong you need to write a date.Try again");
                     else if (formatCorrect == true)
                         applicantDataArr.Add(applicantStartDate);
                 }
 
                 formatCorrect = false;
-                Console.WriteLine("What is the applicant's employment status?" +
+                Console.WriteLine("\nWhat is the applicant's employment status?" +
                     " If the applicant is under 18 then use the parents employment status." +
                     "\nWrite true for employed or false for unemployed");
                 while (formatCorrect == false)
@@ -81,47 +81,47 @@ namespace Arcade_app
                     applicantEmploy = Console.ReadLine();
                     formatCorrect = bool.TryParse(applicantEmploy, out bool Employ);
                     if (formatCorrect == false)
-                        Console.WriteLine("The format is wrong you need to write true or false.");
+                        Console.WriteLine("\nThe format is wrong you need to write true or false.");
                     else if (formatCorrect == true)
                         applicantDataArr.Add(applicantEmploy);
                 }
 
-                Console.WriteLine("What is the applicant's favorite slush-puppy flavor?");
+                Console.WriteLine("\nWhat is the applicant's favorite slush-puppy flavor?");
                 applicantSlushPuppyPref = Console.ReadLine();
                 applicantDataArr.Add(applicantSlushPuppyPref);
 
                 formatCorrect = false;
-                Console.WriteLine("How many sulsh-puppies has the applicant had since first visit?");
+                Console.WriteLine("\nHow many sulsh-puppies has the applicant had since first visit?");
                 while (formatCorrect == false)
                 {
                     applicantSlushPuppyTotal = Console.ReadLine();
                     formatCorrect = int.TryParse(applicantSlushPuppyTotal, out int SlushPuppyTotal);
                     if (formatCorrect == false)
-                        Console.WriteLine("The format is wrong you need to write an number. Try again.");
+                        Console.WriteLine("\nThe format is wrong you need to write an number. Try again.");
                     else if(formatCorrect == true)
                         applicantDataArr.Add(applicantSlushPuppyTotal);
                 }
 
                 formatCorrect = false;
-                Console.WriteLine("What is the applicant's bowling high score rank?");
+                Console.WriteLine("\nWhat is the applicant's bowling high score rank?");
                 while (formatCorrect == false)
                 {
                     applicantBowlHighScore = Console.ReadLine();
                     formatCorrect = int.TryParse(applicantBowlHighScore, out int BowlHighScore);
                     if (formatCorrect == false)
-                        Console.WriteLine("The format is wrong you need to write an number rank.Try again.");
+                        Console.WriteLine("\nThe format is wrong you need to write an number rank.Try again.");
                     else if (formatCorrect == true)
                         applicantDataArr.Add(applicantBowlHighScore);
                 }
 
                 formatCorrect = false;
-                Console.WriteLine("How many pizzas has the applicant eaten since first visit?");
+                Console.WriteLine("\nHow many pizzas has the applicant eaten since first visit?");
                 while (formatCorrect == false)
                 {
                     applicantPizzaTotal = Console.ReadLine();
                     formatCorrect = int.TryParse(applicantPizzaTotal, out int Total);
                     if (formatCorrect == false)
-                        Console.WriteLine("The format is wrong you need to write an number.Try again.");
+                        Console.WriteLine("\nThe format is wrong you need to write an number.Try again.");
                     else if (formatCorrect == true)
                         applicantDataArr.Add(applicantPizzaTotal);
                 }
@@ -130,7 +130,7 @@ namespace Arcade_app
                 applicantData = string.Join(",", applicantDataArr);                                 //This makes the Entered data into a combined string with a "," seperating them
                
                 applicantDataArrEtry.Add(applicantData);                                            //adds the combined string into the next entry of the list to be written into the txt file
-                Console.WriteLine("Do you still want to add anymore applicants? Y for yes and N for no");
+                Console.WriteLine("\nDo you still want to add anymore applicants? Y for yes and N for no");
                 string choice = Console.ReadLine();
                 string choiceUpper = choice.ToUpper();
                 if (choiceUpper == "N")
@@ -145,9 +145,9 @@ namespace Arcade_app
         // Menu creation
         enum Menu
         {
-            CaptureDetails = 1,
-            CheckTokenQualification,
-            Exit
+            Capture_details = 1,
+            View_token_eligibility,
+            Exit_the_program
         }
 
         static void Main(string[] args)
@@ -170,12 +170,14 @@ namespace Arcade_app
             // Perpetual loop to keep program running unless choosing exit
             while (true)
             {
+                Console.Write("========================================================================================================================");
+
                 // Choosing from menu
-                Console.WriteLine("What would you like to do?" +
-                    "\nType the corrosponding number" +
-                    "\n1: Capture details" +
-                    "\n2: See customer token qualification" +
-                    "\n3: Exit program");
+                Console.WriteLine("Menu:");
+                foreach (Menu option in Enum.GetValues(typeof(Menu)))
+                {
+                    Console.WriteLine($"{(int)option}. {option.ToString().Replace('_', ' ')}");
+                }
 
                 // Making sure user input is valid
                 if (!Enum.TryParse(Console.ReadLine(), out Menu optionChosen) || !Enum.IsDefined(typeof(Menu), optionChosen))
@@ -185,19 +187,20 @@ namespace Arcade_app
                 }
 
                 //Exiting program
-                if (optionChosen == Menu.Exit)
+                if (optionChosen == Menu.Exit_the_program)
                 {
                     Environment.Exit(0);
-                    break;
                 }
+
+                Console.WriteLine("========================================================================================================================");
 
                 // Switch case to use the menu option chosen
                 switch (optionChosen)
                 {
-                    case Menu.CaptureDetails:
+                    case Menu.Capture_details:
                         ApplicantDataEntry(filePath);
                         break;
-                    case Menu.CheckTokenQualification:
+                    case Menu.View_token_eligibility:
                         break;
                 }
             }

2e6acb43a3a151b7c0759119da25266686cc5c72
TheMattHattersSon <matthewb.sset@gmail.com>
2024-06-04 16:21:24 +0200

    Update Program.cs
    program with Matt's part, still working on it Will slot it in where needed

--- Program.cs
+++ Program.cs
@@ -4,12 +4,144 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Globalization;
 
 namespace Arcade_app
 {
 
     internal class Program
     {
+        /* 
+    static int MonthCalc(string startdate, DateTime now)
+    {
+        string tempstringNow = DateTime.Now.ToString();
+        string[] nowArray = tempstringNow.Split('/', ' ');
+        string tempstringJoin = startdate;
+        string[] joinArray = tempstringJoin.Split('/');
+        int monthNow = int.Parse(nowArray[1]);
+        int monthStart = int.Parse(joinArray[0]);
+        int yearNow = int.Parse(nowArray[0]);
+        int yearStart = int.Parse(joinArray[3]);
+        int yearDiff = yearNow - yearStart;
+        int monthDiff = monthStart - monthStart;
+        int yearToMonth = yearDiff * 12;
+        int totalMonths = yearToMonth + monthDiff;
+        return totalMonths;
+    }
+
+    static string[,] SearchScoreArrayCreation (string fname, int age, int arcadeScore, int bowlingScore, int averageScore int numUserCounter)
+    {
+        string[,] searchScoreArray;
+        int rowCounter=0;
+        int collumnCounter = 0;
+        for(rowCounter = 0; rowCounter < numUserCounter;rowCounter++)
+            {
+                for(collumnCounter=0;  collumnCounter < 5; collumnCounter++)
+                {
+
+                }
+            }
+
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DateTime.Now.ToString());
+
+        string name;
+        int age;
+        int highScoreRank;
+        string startDateAsLoyalCustomer;
+        int numOfPizzasSinceFirstVisit;
+        int bowlingHS;
+        bool isEmployed;
+        string favoriteSlushieFlavour;
+        int numOfSlushiesSinceFirstVisit;
+        int avgScore;
+        string[] tempArray;
+        string tepExistingApplicant;
+        int i = 0;
+        int j = 0;
+        bool applicationApproved = true;
+        DateTime today = DateTime.Now;
+
+        try
+        {
+         StreamReader reader = new StreamReader(@"C:\Users\matth\source\repos\Program\Applicant_Info.txt");
+         tepExistingApplicant = reader.ReadLine();
+
+        while (tepExistingApplicant != null)
+        {
+         tempArray = tepExistingApplicant.Split(',');
+                    
+         name = tempArray[0];
+         age = int.Parse(tempArray[1]);
+         highScoreRank = int.Parse(tempArray[2]);
+         startDateAsLoyalCustomer = tempArray[3];
+         numOfPizzasSinceFirstVisit = int.Parse(tempArray[4]);
+         bowlingHS = int.Parse(tempArray[5]);
+         isEmployed = bool.Parse(tempArray[6]);
+         favoriteSlushieFlavour = tempArray[7];
+         numOfSlushiesSinceFirstVisit = int.Parse(tempArray[8]);
+         avgScore = int.Parse(tempArray[9]);
+         j++;
+         int loyalCustomerMonths = MonthCalc(startDateAsLoyalCustomer, today);
+
+        //condition cheacks
+         if(isEmployed==false)
+         {
+            applicationApproved = false;
+         }
+         if(applicationApproved==true && loyalCustomerMonths < 24)
+         {
+            applicationApproved = false;
+         }
+         if(applicationApproved == true)
+         {
+            if(highScoreRank <=2000 || bowlingHS <= 1500 ||avgScore <= 1200)
+            {
+                applicationApproved=false;
+            }
+         }
+         if (applicationApproved == true && numOfPizzasSinceFirstVisit / loyalCustomerMonths < 3)
+         {
+            applicationApproved = false;
+         }
+         if (applicationApproved == true && numOfSlushiesSinceFirstVisit / loyalCustomerMonths < 4)
+         {
+            applicationApproved = false;
+         }
+         if(applicationApproved == true && favoriteSlushieFlavour== "Gooey Gulp Galore")
+         {
+            applicationApproved = false;
+         }
+         if (applicationApproved == true)
+         {
+            i++;
+         }
+                //Console.WriteLine("Name: {0}\nAge: {1}\nHigh Score Rank: {2}", name, age, highScoreRank);
+                //Console.WriteLine("Startdate as loyal customer: {0}",startDateAsLoyalCustomer);
+                //Console.WriteLine("number of pizzas bought since first visit: {0}", numOfPizzasSinceFirstVisit);
+                //Console.WriteLine("Bowling High Score: {0}\nIs employed: {1}", bowlingHS, isEmployed);
+                //Console.WriteLine("Favorite Slush-puppy Flavour: {0}", favoriteSlushieFlavour);
+
+        if (applicationApproved == true)
+        {
+            string[,] approvedArray = new string[9, i];
+            for (i)
+            {
+                approvedArray[, j] = tempArray;
+            }
+        }
+                    
+        }
+        }
+        catch (FileNotFoundException)
+        {
+            throw;
+        }
+*/
+
         static void ApplicantDataEntry(string filepath)                             //This is the Method of etering the new applicant data into a .txt file
         {
             string applicantName, applicantAge, applicantHighScoreRank, applicantStartDate, applicantPizzaTotal,

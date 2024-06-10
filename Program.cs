58e3ec76e0a141d3470d60f732217b573e3aa45a
Pandora-boxes <lingogreyling@gmail.com>
2024-06-10 21:49:02 +0200

    Created method ApplicantDataValidation and UpdateData

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Globalization;
 using System.Security.Policy;
+using System.Web;
 
 //Fourie Jooste       (602075)
 //Pandora Greyling    (602369)
@@ -212,18 +213,138 @@ namespace Arcade_app
             }            
         }
 
+        static string ApplicantDataValidation()
+        {
+
+            string applicantName, applicantAge, applicantHighScoreRank, applicantStartDate, applicantPizzaTotal,
+                applicantBowlHighScore, applicantEmploy, applicantSlushPuppyPref, applicantSlushPuppyTotal;
+
+            string applicantData;
+            List<string> applicantDataArr = new List<string>();
+            bool formatCorrect = false;
+
+            Console.WriteLine("What is the applicant's name?");
+            applicantName = Console.ReadLine();
+            applicantDataArr.Add(applicantName);
+            formatCorrect = false;
+
+            Console.WriteLine("\nWhat is the applicant's age?");
+            while (formatCorrect == false)
+            {
+                applicantAge = Console.ReadLine();
+                formatCorrect = int.TryParse(applicantAge, out int Age);            //A tryParse into a throwaway variable like age to
+                                                                                    //check if the string can be properly formatted into it's intended data type
+                if (formatCorrect == false)
+                    Console.WriteLine("\nThe format is wrong you need to write an age. Try again.");
+                else if (formatCorrect == true)
+                    applicantDataArr.Add(applicantAge);
+            }                                                                       //all of these while loops are for format checking
+                                                                                    //so that if the user gets the data format wrong they can redo the data entry
+
+            formatCorrect = false;
+            Console.WriteLine("\nWhat is the applicant's high score rank?");
+            while (formatCorrect == false)
+            {
+                applicantHighScoreRank = Console.ReadLine();
+                formatCorrect = int.TryParse(applicantHighScoreRank, out int HighScoreRank);
+                if (formatCorrect == false)
+                    Console.WriteLine("\nThe format is wrong you need to write an number rank.Try again.");
+                else if (formatCorrect == true)
+                    applicantDataArr.Add(applicantHighScoreRank);
+            }
+
+            formatCorrect = false;
+            Console.WriteLine("\nWhat is the applicant's start date?: mm/dd/yyyy");
+            while (formatCorrect == false)
+            {
+                applicantStartDate = Console.ReadLine();
+                formatCorrect = DateTime.TryParse(applicantStartDate, out DateTime StartDate);
+                if (formatCorrect == false)
+                    Console.WriteLine("\nThe format is wrong you need to write a date.Try again");
+                else if (formatCorrect == true)
+                    applicantDataArr.Add(applicantStartDate);
+            }
+
+            formatCorrect = false;
+            Console.WriteLine("\nWhat is the applicant's employment status?" +
+                " If the applicant is under 18 then use the parents employment status." +
+                "\nY/N");
+            string employ;
+            while (formatCorrect == false)
+            {
+                applicantEmploy = Console.ReadLine();
+                applicantEmploy = applicantEmploy.ToUpper();
+                if (applicantEmploy == "N")
+                {
+                    employ = "false";
+                    applicantDataArr.Add(employ);
+                    formatCorrect = true;
+                }
+                else if (applicantEmploy == "Y")
+                {
+                    employ = "true";
+                    applicantDataArr.Add(employ);
+                    formatCorrect = true;
+                }
+                else
+                {
+                    Console.WriteLine("The format is wrong you need to write Y or N\nTry again:");
+                }
+
+            }
+
+            Console.WriteLine("\nWhat is the applicant's favorite slush-puppy flavor?");
+            applicantSlushPuppyPref = Console.ReadLine();
+            applicantDataArr.Add(applicantSlushPuppyPref);
+
+            formatCorrect = false;
+            Console.WriteLine("\nHow many sulsh-puppies has the applicant had since first visit?");
+            while (formatCorrect == false)
+            {
+                applicantSlushPuppyTotal = Console.ReadLine();
+                formatCorrect = int.TryParse(applicantSlushPuppyTotal, out int SlushPuppyTotal);
+                if (formatCorrect == false)
+                    Console.WriteLine("\nThe format is wrong you need to write an number. Try again.");
+                else if (formatCorrect == true)
+                    applicantDataArr.Add(applicantSlushPuppyTotal);
+            }
+
+            formatCorrect = false;
+            Console.WriteLine("\nWhat is the applicant's bowling high score rank?");
+            while (formatCorrect == false)
+            {
+                applicantBowlHighScore = Console.ReadLine();
+                formatCorrect = int.TryParse(applicantBowlHighScore, out int BowlHighScore);
+                if (formatCorrect == false)
+                    Console.WriteLine("\nThe format is wrong you need to write an number rank.Try again.");
+                else if (formatCorrect == true)
+                    applicantDataArr.Add(applicantBowlHighScore);
+            }
+
+            formatCorrect = false;
+            Console.WriteLine("\nHow many pizzas has the applicant eaten since first visit?");
+            while (formatCorrect == false)
+            {
+                applicantPizzaTotal = Console.ReadLine();
+                formatCorrect = int.TryParse(applicantPizzaTotal, out int Total);
+                if (formatCorrect == false)
+                    Console.WriteLine("\nThe format is wrong you need to write an number.Try again.");
+                else if (formatCorrect == true)
+                    applicantDataArr.Add(applicantPizzaTotal);
+            }
+
+
+            applicantData = string.Join(",", applicantDataArr);                                 //This makes the Entered data into a combined string with a "," seperating them
+            return applicantData;
+        }
 
         static void ApplicantDataEntry(string filepath, List<string> applicantDataEtry)               //This is the Method of etering the new applicant data into a .txt file
         {
-            string applicantName, applicantAge, applicantHighScoreRank, applicantStartDate, applicantPizzaTotal,
-                applicantBowlHighScore, applicantEmploy, applicantSlushPuppyPref, applicantSlushPuppyTotal; 
-
+          
             string applicantData;
             List<string> applicantDataArr = new List<string>();                     //list to hold the data as its being entered
             
-            bool formatCorrect=false;
             bool enter = true;
-
            
                                                                                           //a do while loop to contain the user so that if they want to repeat the data
                                                                                         //entry then they can with a simple bool loop
@@ -231,134 +352,42 @@ namespace Arcade_app
             {
                 Console.Clear();
                 applicantDataArr.Clear();
-                Console.WriteLine("What is the applicant's name?");
-                applicantName = Console.ReadLine();
-                applicantDataArr.Add(applicantName);
-                formatCorrect = false;
 
-                Console.WriteLine("\nWhat is the applicant's age?");
-                while (formatCorrect == false)
-                {
-                    applicantAge = Console.ReadLine();
-                    formatCorrect = int.TryParse(applicantAge, out int Age);            //A tryParse into a throwaway variable like age to
-                                                                                        //check if the string can be properly formatted into it's intended data type
-                    if (formatCorrect == false)
-                        Console.WriteLine("\nThe format is wrong you need to write an age. Try again.");
-                    else if (formatCorrect == true)
-                        applicantDataArr.Add(applicantAge);
-                }                                                                       //all of these while loops are for format checking
-                                                                                        //so that if the user gets the data format wrong they can redo the data entry
-
-                formatCorrect = false;
-                Console.WriteLine("\nWhat is the applicant's high score rank?");
-                while (formatCorrect == false)
-                { 
-                    applicantHighScoreRank = Console.ReadLine();
-                    formatCorrect = int.TryParse(applicantHighScoreRank, out int HighScoreRank);
-                    if (formatCorrect == false)
-                        Console.WriteLine("\nThe format is wrong you need to write an number rank.Try again.");
-                    else if (formatCorrect == true)
-                        applicantDataArr.Add(applicantHighScoreRank);
-                }
-
-                formatCorrect = false;
-                Console.WriteLine("\nWhat is the applicant's start date?: mm/dd/yyyy");     
-                while (formatCorrect == false)
-                {
-                    applicantStartDate = Console.ReadLine();
-                    formatCorrect = DateTime.TryParse(applicantStartDate, out DateTime StartDate);
-                    if (formatCorrect == false)
-                        Console.WriteLine("\nThe format is wrong you need to write a date.Try again");
-                    else if (formatCorrect == true)
-                        applicantDataArr.Add(applicantStartDate);
-                }
-
-                formatCorrect = false;
-                Console.WriteLine("\nWhat is the applicant's employment status?" +
-                    " If the applicant is under 18 then use the parents employment status." +
-                    "\nY/N");
-                string employ;
-                while (formatCorrect == false)
-                {
-                    applicantEmploy = Console.ReadLine();
-                    applicantEmploy = applicantEmploy.ToUpper();
-                    if (applicantEmploy == "N")
-                    {
-                        employ = "false";
-                        applicantDataArr.Add(employ);
-                        formatCorrect = true;
-                    }
-                    else if (applicantEmploy == "Y") 
-                    {
-                        employ = "true";
-                        applicantDataArr.Add(employ);
-                        formatCorrect = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("The format is wrong you need to write Y or N\nTry again:");
-                    }
-                
-                }
-
-                Console.WriteLine("\nWhat is the applicant's favorite slush-puppy flavor?");
-                applicantSlushPuppyPref = Console.ReadLine();
-                applicantDataArr.Add(applicantSlushPuppyPref);
-
-                formatCorrect = false;
-                Console.WriteLine("\nHow many sulsh-puppies has the applicant had since first visit?");
-                while (formatCorrect == false)
-                {
-                    applicantSlushPuppyTotal = Console.ReadLine();
-                    formatCorrect = int.TryParse(applicantSlushPuppyTotal, out int SlushPuppyTotal);
-                    if (formatCorrect == false)
-                        Console.WriteLine("\nThe format is wrong you need to write an number. Try again.");
-                    else if(formatCorrect == true)
-                        applicantDataArr.Add(applicantSlushPuppyTotal);
-                }
-
-                formatCorrect = false;
-                Console.WriteLine("\nWhat is the applicant's bowling high score rank?");
-                while (formatCorrect == false)
-                {
-                    applicantBowlHighScore = Console.ReadLine();
-                    formatCorrect = int.TryParse(applicantBowlHighScore, out int BowlHighScore);
-                    if (formatCorrect == false)
-                        Console.WriteLine("\nThe format is wrong you need to write an number rank.Try again.");
-                    else if (formatCorrect == true)
-                        applicantDataArr.Add(applicantBowlHighScore);
-                }
-
-                formatCorrect = false;
-                Console.WriteLine("\nHow many pizzas has the applicant eaten since first visit?");
-                while (formatCorrect == false)
-                {
-                    applicantPizzaTotal = Console.ReadLine();
-                    formatCorrect = int.TryParse(applicantPizzaTotal, out int Total);
-                    if (formatCorrect == false)
-                        Console.WriteLine("\nThe format is wrong you need to write an number.Try again.");
-                    else if (formatCorrect == true)
-                        applicantDataArr.Add(applicantPizzaTotal);
-                }
-              
-
-                applicantData = string.Join(",", applicantDataArr);                                 //This makes the Entered data into a combined string with a "," seperating them
-
+                applicantData = ApplicantDataValidation();
                 applicantDataEtry.Add(applicantData);                                            //adds the combined string into the next entry of the list to be written into the txt file
+
                 Console.WriteLine("\nDo you still want to add anymore applicants? (Y/N)");
                 string choice = Console.ReadLine();
                 string choiceUpper = choice.ToUpper();
                 if (choiceUpper == "N")
                     enter = false;
-                
-                
 
             } while (enter == true);
+
             StartAnimation();
             File.WriteAllLines(filepath, applicantDataEtry);                                    //writes the applicants in the array and recently added into the file
             StopAnimation();
         }
         
+
+        static string UpdateData(List<string> dataArray, string filepath, string name, string age)
+        {
+            for(int i = 0; i < dataArray.Count ; i++) 
+            {
+                List<string> lineArr = new List<string>(dataArray[i].Split(','));
+                if (lineArr.Contains<string>(name) == true)
+                    if (lineArr.Contains<string>(age) == true) 
+                    {
+                        string newData = ApplicantDataValidation();
+                        dataArray[i] = newData;
+                        StartAnimation();
+                        File.WriteAllLines(filepath, dataArray);
+                        StopAnimation() ;
+                        return "The data has been updated sucessfully.";
+                    }
+            }
+            return "The user hasn't been found in the data.";
+        }
 
         // Menu creation
         enum Menu
@@ -395,7 +424,7 @@ namespace Arcade_app
                 if (filepatharr[i] == "Arcade_app")                                     //checks for the start of the Arcade_App file
                 {
                     filepatharr[i + 1] = "ApplicantData.txt";                           //changes the next cell after the start to the file name
-                    filepatharr.Remove("Debug");                                        //removes the debug from the 
+                    filepatharr.Remove("Debug");                                        //removes the debug from the directory
                     break;
                 }
 
